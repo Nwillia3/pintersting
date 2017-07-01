1d371b78f75ebdfc5ba1694a64b6107945a33bbc
Neil Williams <neil.williams227@gmail.com>
2017-07-01 12:56:18 -0500

    users added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  extend Devise::Models
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :password, :presence => true,
@@ -8,9 +10,8 @@ class User < ApplicationRecord
   										 :if => :password,
   										 :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 
-
-
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :pins
 end
