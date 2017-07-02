b1335fa63e63b51ec904b4964ec965e12744d205
Neil Williams <neil.williams227@gmail.com>
2017-07-02 09:42:45 -0500

    fixing registration & bell w/ whistles

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,8 @@ class Pin < ApplicationRecord
 										:default_url => "/images/:style/missing.png" # <= you see ?
 
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+	validates :image, presence: true
+  validates :description, presence: true
+
 end
